4b0f4fdbb88db470882c35cc111282d156166253
Ralph <rafael.ortiz1997@yahoo.com>
2023-07-19 15:35:16 -0700

    completed

--- Develop/script.js
+++ Develop/script.js
@@ -1,31 +1,47 @@
 // Assignment code here
 function generatePassword() {
-  var pw = "password"
+  var result = "";
+  const lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  const upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  const nums = ["0","1","2","3","4","5","6","7","8","9"];
+  const special = [" ","!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"];
+  var charCode = [];
 
   do {
     var pwLength = prompt("How many characters would you like your passwoed to be? (8-128)");
   } while (pwLength < 8 || pwLength > 128 || /^\d+$/.test(pwLength) == false);
   do {
     var pwLower = prompt("Would you like your password to contain lower case letters? (Y/N)");
-  } while (pwLower != "Y" && pwLower != "y" && pwLower != "N" && pwLower != "N");
+  } while (pwLower != "Y" && pwLower != "y" && pwLower != "N" && pwLower != "n");
   do {
     var pwUpper = prompt("Would you like your password to contain upper case letters? (Y/N)");
-  } while (pwUpper != "Y" && pwUpper != "y" && pwUpper != "N" && pwUpper != "N");
+  } while (pwUpper != "Y" && pwUpper != "y" && pwUpper != "N" && pwUpper != "n");
   do {
     var pwNums = prompt("Would you like your password to contain numbers? (Y/N)");
-  } while (pwLower != "Y" && pwLower != "y" && pwLower != "N" && pwLower != "N");
+  } while (pwNums != "Y" && pwNums != "y" && pwNums != "N" && pwNums != "n");
   do {
     var pwSpecialChar = prompt("Would you like your password to contain special characters? (Y/N)");
-  } while (pwLower != "Y" && pwLower != "y" && pwLower != "N" && pwLower != "N");
+  } while (pwSpecialChar != "Y" && pwSpecialChar != "y" && pwSpecialChar != "N" && pwSpecialChar != "n");
+
+  if (pwLower == "Y" || pwLower == "y") {
+    charCode = charCode.concat(lower);
+  }
+  if (pwUpper == "Y" || pwUpper == "y") {
+    charCode = charCode.concat(upper);
+  }
+  if (pwNums == "Y" || pwNums == "y") {
+    charCode = charCode.concat(nums);
+  }
+  if (pwSpecialChar == "Y" || pwSpecialChar == "y") {
+    charCode = charCode.concat(special);
+  }
   
-  
-  console.log(pwUpper);
-  console.log(pwNums);
-  console.log(pwSpecialChar);
+  for(let i = 0; i < pwLength; i++) {
+    result += charCode[Math.floor(Math.random() * charCode.length)];
+  }
+  console.log(result);
 
-
-
-  return pw;
+  return result;
 }
 
 // Get references to the #generate element
